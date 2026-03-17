822cf14a4e9bc7db4dd1d77150a5f7f82063571f
Icobart <enrico.cornacchia@studio.unibo.it>
2026-03-17 14:22:01 +0100

    working on task 1 optional, implemented contains

--- src/main/scala/it/unibo/pps/u03/Sequences.scala
+++ src/main/scala/it/unibo/pps/u03/Sequences.scala
@@ -142,7 +142,10 @@ object Sequences: // Essentially, generic linkedlists
      * E.g., [10, 20, 30] => true if elem is 20
      * E.g., [10, 20, 30] => false if elem is 40
      */
-    def contains[A](s: Sequence[A])(elem: A): Boolean = ???
+    def contains[A](s: Sequence[A])(elem: A): Boolean = s match
+      case Cons(h, t) if h.equals(elem) => true
+      case Cons(_, t) => contains(t)(elem)
+      case _ => false
 
     /*
      * Remove duplicates from the sequence
